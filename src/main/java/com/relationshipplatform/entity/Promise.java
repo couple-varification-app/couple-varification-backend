71befe20e77e22861eda6e419dcf6a73e841acd9
riteshdawale02 <riteshdawale1596@gmail.com>
2026-02-09 21:41:15 +0530

    updated promise entity

--- src/main/java/com/relationshipplatform/entity/Promise.java
+++ src/main/java/com/relationshipplatform/entity/Promise.java
@@ -1,11 +1,16 @@
 package com.relationshipplatform.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -31,5 +36,23 @@ public class Promise {
     private boolean user2Approved;
 
     private String status; // ACTIVE, BROKEN, COMPLETED
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDate expiryDate;
+    
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
 
